7aeb8a533f28f512a1bc932f5e6e847e616207a6
IanClifer <107898194+IanClifer@users.noreply.github.com>
2023-04-20 14:03:08 +0800

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter hours (0-11): ");      //This part read the user's input and use int.Parse
+            int hours = int.Parse(Console.ReadLine());  //to convert it to an integer.
+
+            Console.Write("Enter minutes (0-59): ");        //Same as the above but for minutes.
+            int minutes = int.Parse(Console.ReadLine());    //Both need to start from 0 because the clock start from 0.
+
+            double minuteAngle = minutes * 6;                   //This part took me a while. It's my first time messing with shapes in programming.
+            double hourAngle = (hours + (minutes / 60)) * 30;   //But this is exciting enough to not use google and use only my brain to figure this out.
+                                                                //I had to divide 360 (circle) into 60 (the minutes) to find the exact angles per minute.
+                                                                //Same for the hours. Had to divide by 30. I needed to divide the minutes into 60 and add it to hours.
+                                                                //That way I can find the exact position of the hour arm since the arm moves constantly too while the minute arm moves.
+                                                                //So if it's 3:30 this formula will give me 3.5 which is the exact position of the hour arm at that point in time.
+
+            double angle = Math.Abs(minuteAngle - hourAngle);   //This is the first time I used this Math.Abs method. I learned (through google) that this can be used
+                                                                //to return the positive value of any number you feed to its parameter.
+
+            if (angle > 180)            //So there are two angles that can be calculated.
+            {                           //This make sure to always calculate the lesser angle.
+                angle = 360 - angle;
+            }
+
+            Console.Write("The angle is: " + angle);    //Output the result.
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        class Branch
+        {
+            public List<Branch> branches = new();   //Need to add = new(); since it will throw a null error if it's not included.
+        }
+
+        static void Main(string[] args)
+        {
+            Branch root = new Branch();                             //Starting from this part, I created `Branch` objects and add child nodes into them.
+            root.branches.Add(new Branch());                        //I replicated the structure of the figure provided in the assignment. It has a depth of 5.
+            root.branches.Add(new Branch());
+
+            root.branches[0].branches.Add(new Branch());
+            root.branches[1].branches.Add(new Branch());
+            root.branches[1].branches.Add(new Branch());
+            root.branches[1].branches.Add(new Branch());
+            root.branches[1].branches[0].branches.Add(new Branch());
+            root.branches[1].branches[1].branches.Add(new Branch());
+            root.branches[1].branches[1].branches.Add(new Branch());
+            root.branches[1].branches[1].branches[0].branches.Add(new Branch());
+
+            int depth = CalculateDepth(root);                   //Call the method I created and feed the structure into it.
+            Console.WriteLine("Depth of structure: " + depth);  //Output the result.
+        }
+
+        static int CalculateDepth(Branch branch)    //The method for calculating depth of a `Branch`.
+        {
+            if (branch.branches.Count == 0)         //Checks if it has any child nodes.
+            {
+                return 1;
+            }
+            else
+            {
+                int maxDepth = 0;                                   //If it has, it loops through the branches list of the current Branch object,
+                foreach (Branch subBranch in branch.branches)       //and calling CalculateDepth on each child node. It then takes the maximum depth of all the child nodes,
+                {
+                    int subDepth = CalculateDepth(subBranch);       //Recursion.
+                    if (subDepth > maxDepth)
+                    {
+                        maxDepth = subDepth;
+                    }
+                }
+                return maxDepth + 1;    //and add 1 for the current Branch object since it is zero-based.
+            }
+        }
+    }
+}
+
